35b462a63486fe84dfd6ea2661fb8715c61fc16b
Dhruvagwal <dhruv@clus.in>
2021-05-01 19:30:37 +0530

    UI_Commit

---
+++ src/screen/service/ServiceDescription.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ServiceDescription = () => {
+    return (
+        <View>
+            <Text>Description</Text>
+        </View>
+    )
+}
+
+export default ServiceDescription
+
+const styles = StyleSheet.create({})
